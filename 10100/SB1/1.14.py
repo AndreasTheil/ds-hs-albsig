f819c5cf6e3ab64151aee2d7ca5ba7debb37ef04
Sergej Schweizer <sergej.schweizer@gmail.com>
2018-09-29 22:20:06 +0200

    dailey changes

---
+++ 10100/SB1/1.14.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 19:23:34 2018
+
+@author: Sergej Schweizer 
+@mail:   sergej.schweizer@gmail.com
+
+"""
+
+print(
+
+     any(map(
+             lambda zeile: 
+                 list(map(
+                         lambda zeile2: zeile2 if (len(zeile2)>2 and zeile2[1].isupper()) else False,
+                         zeile.split()
+                        )   
+                    ) 
+        ,f.splitlines() )
+      )
+    )
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    
---
+++ 10100/SB1/1.17.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 22:03:02 2018
+
+@author: Sergej Schweizer 
+@mail:   sergej.schweizer@gmail.com
+
+"""
+
+ [  lines.split()[0] for lines in f.splitlines() if len(lines) > 0 ]
+ [ lines for lines in f.splitlines() if len(lines) > 0 and lines[0].islower() ]
+ max([ len([ word for word in words.split() if word.islower() ])  for words in f.splitlines() ] )
