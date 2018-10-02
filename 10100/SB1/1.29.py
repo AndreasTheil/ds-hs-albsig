56c461aa13d95d7ccded01284dfcb2175b675df7
Sergej Schweizer <sergej.schweizer@gmail.com>
2018-10-02 20:53:20 +0200

    1.29c

--- 10100/SB1/1.29.py
+++ 10100/SB1/1.29.py
@@ -14,3 +14,9 @@ list(max([[ (file, len(file)) for file in  files ] for cur,dirs,files in walk('.
 
 # 1.29b
 list(max([[ (file, file.count('e')) for file in  files ] for cur,dirs,files in walk('.') ]))
+
+
+# 1.29c
+max(max(max(list(filter(list, [ [ [ (len(l),l,f) for l in open(d+'/'+f, mode='rb').read().splitlines() ]  for f in  fs if f.endswith('txt')  ] for d,ds,fs in walk('.') ])))))
+
+max(max(list(filter(list, [ [ (len(open(d+'/'+f, mode='rb').read().splitlines()),d+'/'+f) for f in fs if f.endswith('txt') ] for d,ds,fs in walk('.') ]))))
